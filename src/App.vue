00bea692b07039033973815576b058353fcff90b
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-11-17 09:17:05 -0500

    First

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+  
+  <script>
+import AppHeader from './components/AppHeader.vue'
+import AppSearch from './components/AppSearch.vue'
+import CharacterList from './components/CharacterList.vue';
+  
+  export default {
+  name: 'App',
+  components: { AppHeader, AppSearch, CharacterList }
+  }
+  </script>
+
+<template>
+  <div class="container">
+    <AppHeader />
+    <main>
+      <AppSearch/>
+      <CharacterList/>
+    </main>
+  </div>
+</template>
+
+<style lang="scss">
+  @use './styles/general' as *;
+</style>
---
+++ src/components/CharacterList.vue
@@ -0,0 +1,14 @@
+  <script>
+  export default {
+  
+  }
+  </script>
+
+<template>
+  <h1>C</h1>
+</template>
+
+
+<style>
+
+</style>
