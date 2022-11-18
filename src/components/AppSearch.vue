9764d814896fb8dc1ecb0f004c1b015ba9f9ef3c
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-11-18 09:03:41 -0500

    [add] select into appsearch

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -5,10 +5,21 @@
   </script>
 
 <template>
-  <p>Select Category</p>
+  <div class="container">
+    <div class="row">
+      <div class="col-auto py-5">
+        <select class="form-select" aria-label="Default select example">
+        <option selected>Select Category</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+      </div>
+    </div>
+  </div>
 </template>
 
 
-<style>
+<style lang="scss" scoped>
 
 </style>
